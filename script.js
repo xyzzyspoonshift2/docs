b7ad262a7fcf3690ab4d4029e233cec967640129
xyzzyspoonshift2 <rijul2608jain@gmail.com>
2026-01-22 22:23:51 +0530

    Add smooth scroll to services section on button click

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const getServicesBtn = document.getElementById("getServicesBtn");
+const servicesSection = document.getElementById("services");
 
+getServicesBtn.addEventListener("click", () => {
+  servicesSection.scrollIntoView({ behavior: "smooth" });
+});
